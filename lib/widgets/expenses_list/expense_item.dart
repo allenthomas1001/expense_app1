19a20f5cfaf2b03083e546379a8974369a4b3a17
allenthomas1001 <allenthomas1001@gmail.com>
2024-12-10 10:00:02 +0530

    none

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -16,18 +16,21 @@ class ExpenseItem extends StatelessWidget {
           vertical: 16,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(expense.title, style: Theme.of(context).textTheme.titleLarge),
             const SizedBox(height: 4),
             Row(
               children: [
-                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                Text('\₹${expense.amount.toStringAsFixed(2)}',
+                    style: Theme.of(context).textTheme.titleLarge),
                 const Spacer(),
                 Row(
                   children: [
                     Icon(categoryIcons[expense.category]),
                     const SizedBox(width: 8),
-                    Text(expense.formattedDate),
+                    Text(expense.formattedDate,
+                        style: Theme.of(context).textTheme.titleLarge),
                   ],
                 ),
               ],
